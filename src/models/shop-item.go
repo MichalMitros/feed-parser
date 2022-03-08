d2dc584a99288a3677385b219800c0a74c1258d2
MichalMitros <mitrosmicha4@gmail.com>
2022-03-08 21:31:07 +0100

    add json tags to ShopItem model

--- src/models/shop-item.go
+++ src/models/shop-item.go
@@ -3,57 +3,57 @@ package models
 import "encoding/xml"
 
 type ShopItem struct {
-	XMLName           xml.Name                   `xml:"SHOPITEM"`
-	Text              string                     `xml:",chardata"`
-	ItemID            string                     `xml:"ITEM_ID"`
-	ProductName       string                     `xml:"PRODUCTNAME"`
-	Product           string                     `xml:"PRODUCT"`
-	Description       string                     `xml:"DESCRIPTION"`
-	Url               string                     `xml:"URL"`
-	ImgUrl            string                     `xml:"IMGURL"`
-	ImgUrlAlternative string                     `xml:"IMGURL_ALTERNATIVE"`
-	VideoUrl          string                     `xml:"VIDEO_URL"`
-	PriceVat          string                     `xml:"PRICE_VAT"`
-	HeurekaCPC        string                     `xml:"HEUREKA_CPC"`
-	CategoryText      string                     `xml:"CATEGORYTEXT"`
-	EAN               string                     `xml:"EAN"`
-	ProductNo         string                     `xml:"PRODUCTNO"`
-	Params            []ShopItemParam            `xml:"PARAM"`
-	DelivaryDate      string                     `xml:"DELIVERY_DATE"`
-	Deliveries        []ShopItemDelivery         `xml:"DELIVERY"`
-	ItemGroupId       string                     `xml:"ITEMGROUP_ID"`
-	Accessory         string                     `xml:"ACCESSORY"`
-	Gift              string                     `xml:"GIFT"`
-	ExtendedWarranty  []ShopItemExtendedWarranty `xml:"EXTENDED_WARRANTY"`
-	SpecialService    string                     `xml:"SPECIAL_SERVICE"`
-	SalesVoucher      []ShopItemSalesVoucher     `xml:"SALES_VOUCHER"`
+	XMLName           xml.Name                   `xml:"SHOPITEM" json:"-"`
+	Text              string                     `xml:",chardata" json:"-"`
+	ItemID            string                     `xml:"ITEM_ID" json:"itemId"`
+	ProductName       string                     `xml:"PRODUCTNAME" json:"productName"`
+	Product           string                     `xml:"PRODUCT" json:"product"`
+	Description       string                     `xml:"DESCRIPTION" json:"description"`
+	Url               string                     `xml:"URL" json:"url"`
+	ImgUrl            string                     `xml:"IMGURL" json:"imgUrl"`
+	ImgUrlAlternative string                     `xml:"IMGURL_ALTERNATIVE" json:"imgUrlAlternative"`
+	VideoUrl          string                     `xml:"VIDEO_URL" json:"videoUrl"`
+	PriceVat          string                     `xml:"PRICE_VAT" json:"priceVAT"`
+	HeurekaCPC        string                     `xml:"HEUREKA_CPC" json:"heurekaCPC"`
+	CategoryText      string                     `xml:"CATEGORYTEXT" json:"categoryText"`
+	EAN               string                     `xml:"EAN" json:"ean"`
+	ProductNo         string                     `xml:"PRODUCTNO" json:"productNo"`
+	Params            []ShopItemParam            `xml:"PARAM" json:"param"`
+	DelivaryDate      string                     `xml:"DELIVERY_DATE" json:"deliveryDate"`
+	Deliveries        []ShopItemDelivery         `xml:"DELIVERY" json:"deliveries"`
+	ItemGroupId       string                     `xml:"ITEMGROUP_ID" json:"itemGroupId"`
+	Accessory         string                     `xml:"ACCESSORY" json:"accessory"`
+	Gift              string                     `xml:"GIFT" json:"gift"`
+	ExtendedWarranty  []ShopItemExtendedWarranty `xml:"EXTENDED_WARRANTY" json:"extendedWarranty"`
+	SpecialService    string                     `xml:"SPECIAL_SERVICE" json:"specialService"`
+	SalesVoucher      []ShopItemSalesVoucher     `xml:"SALES_VOUCHER" json:"salesVoucher"`
 }
 
 type ShopItemParam struct {
-	XMLName   xml.Name `xml:"PARAM"`
-	Text      string   `xml:",chardata"`
-	ParamName string   `xml:"PARAM_NAME"`
-	Val       string   `xml:"VAL"`
+	XMLName   xml.Name `xml:"PARAM" json:"-"`
+	Text      string   `xml:",chardata" json:"-"`
+	ParamName string   `xml:"PARAM_NAME" json:"paramName"`
+	Val       string   `xml:"VAL" json:"val"`
 }
 
 type ShopItemDelivery struct {
-	XMLName          xml.Name `xml:"DELIVERY"`
-	Text             string   `xml:",chardata"`
-	DeliveryID       string   `xml:"DELIVERY_ID"`
-	DeliveryPrice    string   `xml:"DELIVERY_PRICE"`
-	DeliveryPriceCOD string   `xml:"DELIVERY_PRICE_COD"`
+	XMLName          xml.Name `xml:"DELIVERY" json:"-"`
+	Text             string   `xml:",chardata" json:"-"`
+	DeliveryID       string   `xml:"DELIVERY_ID" json:"deliveryId"`
+	DeliveryPrice    string   `xml:"DELIVERY_PRICE" json:"deliveryPrice"`
+	DeliveryPriceCOD string   `xml:"DELIVERY_PRICE_COD" json:"deliveryPriceCOD"`
 }
 
 type ShopItemExtendedWarranty struct {
-	XMLName xml.Name `xml:"EXTENDED_WARRANTY"`
-	Text    string   `xml:",chardata"`
-	Val     string   `xml:"VAL"`
-	Desc    string   `xml:"DESC"`
+	XMLName xml.Name `xml:"EXTENDED_WARRANTY" json:"-"`
+	Text    string   `xml:",chardata" json:"-"`
+	Val     string   `xml:"VAL" json:"val"`
+	Desc    string   `xml:"DESC" json:"desc"`
 }
 
 type ShopItemSalesVoucher struct {
-	XMLName xml.Name `xml:"SALES_VOUCHER"`
-	Text    string   `xml:",chardata"`
-	Code    string   `xml:"CODE"`
-	Desc    string   `xml:"DESC"`
+	XMLName xml.Name `xml:"SALES_VOUCHER" json:"-"`
+	Text    string   `xml:",chardata" json:"-"`
+	Code    string   `xml:"CODE" json:"code"`
+	Desc    string   `xml:"DESC" json:"desc"`
 }
--- src/models/shop.go
+++ src/models/shop.go
@@ -3,7 +3,7 @@ package models
 import "encoding/xml"
 
 type Shop struct {
-	XMLName   xml.Name   `xml:"SHOP"`
-	Text      string     `xml:",chardata"`
-	ShopItems []ShopItem `xml:"SHOPITEM"`
+	XMLName   xml.Name   `xml:"SHOP" json:"-"`
+	Text      string     `xml:",chardata" json:"-"`
+	ShopItems []ShopItem `xml:"SHOPITEM" json:"shopItems"`
 }
