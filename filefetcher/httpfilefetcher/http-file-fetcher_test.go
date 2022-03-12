0042745e34d634c6dbd9e5af6cdacb3397162241
MichalMitros <mitrosmicha4@gmail.com>
2022-03-12 01:42:43 +0100

    add more tests of http fetcher and xml parser

--- filefetcher/httpfilefetcher/http-file-fetcher_test.go
+++ filefetcher/httpfilefetcher/http-file-fetcher_test.go
@@ -1,41 +1,227 @@
 package httpfilefetcher
 
 import (
+	"encoding/xml"
+	"io"
 	"net/http"
+	"reflect"
+	"strings"
 	"testing"
+
+	"github.com/MichalMitros/feed-parser/models"
 )
-
-// Mocked http.Client as struct implementing FileFetcher interface
-type MockedHttpClient struct{}
-
-func (c MockedHttpClient) Get(url string) (*http.Response, error) {
-	return &http.Response{
-		Status:     "SOME STATUS",
-		StatusCode: 200,
-		Body:       MockedHttpResponseBody{},
-	}, nil
-}
-
-type MockedHttpResponseBody struct{}
-
-func (b MockedHttpResponseBody) Read(p []byte) (int, error) {
-
-	return 0, nil
-}
-
-func (b MockedHttpResponseBody) Close() error {
-	return nil
-}
 
 func TestFetchFile(t *testing.T) {
 	client := MockedHttpClient{}
 	filesFetcher := NewHttpFileFetcher(
 		client,
 	)
-	files, _, err := filesFetcher.FetchFile("url")
+	result, _, err := filesFetcher.FetchFile("some_test_url")
 
+	// Check error
 	if err != nil {
-		t.Fatalf(`FetchFiles([]string{}) = %q, %v, want []string{""}, nil`, files, err)
+		t.Fatalf(`FetchFiles(string), err = %v, want nil`, err)
 	}
 
+	// Check if returned io.ReadCloser isn't changed
+	if !reflect.DeepEqual(mockedReadCloser, result) {
+		t.Fatalf(
+			"FetchFiles(string), should return unchanged io.ReadCloser",
+		)
+	}
+}
+
+func TestDefaultHttpFileFetcher(t *testing.T) {
+	// Create default fetcher
+	fetcher := DefaultHttpFileFetcher()
+
+	// Check if returned io.ReadCloser isn't changed
+	if !reflect.DeepEqual(fetcher.httpClient, http.DefaultClient) {
+		t.Fatalf(
+			"DefaultHttpFileFetcher(), should return fetcher with http.DefaultClient",
+		)
+	}
+}
+
+func TestNewHttpFileFetcher(t *testing.T) {
+	// Create default fetcher
+	fetcher := NewHttpFileFetcher(http.DefaultClient)
+
+	// Check if returned io.ReadCloser isn't changed
+	if !reflect.DeepEqual(fetcher.httpClient, http.DefaultClient) {
+		t.Fatalf(
+			"DefaultHttpFileFetcher(), should return fetcher with HttpClientInterface from the input",
+		)
+	}
+}
+
+// MOCKED DATA
+
+// Mocked http.Client as struct implementing FileFetcher interface
+type MockedHttpClient struct{}
+
+func (c MockedHttpClient) Get(url string) (*http.Response, error) {
+	return &http.Response{
+		Status:     "200 OK",
+		StatusCode: 200,
+		Body:       mockedReadCloser,
+	}, nil
+}
+
+// io.ReadCloser with mockedCorrectShop
+var mockedXmlFileBytes, _ = xml.Marshal(mockedCorrectShop)
+var mockedReadCloser = io.NopCloser(strings.NewReader(string(mockedXmlFileBytes)))
+
+// Correct shop with 3 items
+var mockedCorrectShop models.Shop = models.Shop{
+	ShopItems: []models.ShopItem{
+		{
+			XMLName:           xml.Name{Local: "SHOPITEM"},
+			ItemID:            "testId_1",
+			ProductName:       "testProductName_1",
+			Product:           "testProduct_1",
+			Description:       "testDescription_1",
+			Url:               "testUrl_1",
+			ImgUrl:            "testImgUrl_1",
+			ImgUrlAlternative: "testImgUrlAlt_1",
+			VideoUrl:          "testVideoUrl_1",
+			PriceVat:          "testPriceVat_1",
+			HeurekaCPC:        "testHeurekaCPC_1",
+			CategoryText:      "testCategoryText_1",
+			EAN:               "testEAN_1",
+			ProductNo:         "testProductNo_1",
+			Params: []models.ShopItemParam{
+				{XMLName: xml.Name{Local: "PARAM"}, ParamName: "testParam_1_1", Val: "testVal_1_1"},
+				{XMLName: xml.Name{Local: "PARAM"}, ParamName: "testParam_1_2", Val: "testVal_1_2"},
+			},
+			DelivaryDate: "testDelDate_1",
+			Deliveries: []models.ShopItemDelivery{
+				{XMLName: xml.Name{Local: "DELIVERY"}, DeliveryID: "testDelId_1_1", DeliveryPrice: "testDelPrice_1_1", DeliveryPriceCOD: "testDelPriceCOD_1_1"},
+				{XMLName: xml.Name{Local: "DELIVERY"}, DeliveryID: "testDelId_1_2", DeliveryPrice: "testDelPrice_1_2", DeliveryPriceCOD: "testDelPriceCOD_1_2"},
+			},
+			ItemGroupId: "testGroupId_1",
+			Accessory:   "testAccesory_1",
+			Gift:        "testGift_1",
+			ExtendedWarranty: []models.ShopItemExtendedWarranty{
+				{XMLName: xml.Name{Local: "EXTENDED_WARRANTY"}, Val: "testWarrantyVal_1_1", Desc: "testWarrantyDesc_1_1"},
+				{XMLName: xml.Name{Local: "EXTENDED_WARRANTY"}, Val: "testWarrantyVal_1_2", Desc: "testWarrantyDesc_1_2"},
+				{XMLName: xml.Name{Local: "EXTENDED_WARRANTY"}, Val: "testWarrantyVal_1_3", Desc: "testWarrantyDesc_1_3"},
+			},
+			SpecialService: "testSpecSvc_1",
+			SalesVoucher: []models.ShopItemSalesVoucher{
+				{XMLName: xml.Name{Local: "SALES_VOUCHER"}, Code: "testVoucherCode_1_1", Desc: "testVoucherDesc_1_1"},
+			},
+		},
+		{
+			XMLName:           xml.Name{Local: "SHOPITEM"},
+			ItemID:            "testId_2",
+			ProductName:       "testProductName_2",
+			Product:           "testProduct_2",
+			Description:       "testDescription_2",
+			Url:               "testUrl_2",
+			ImgUrl:            "testImgUrl_2",
+			ImgUrlAlternative: "testImgUrlAlt_2",
+			VideoUrl:          "testVideoUrl_2",
+			PriceVat:          "testPriceVat_2",
+			HeurekaCPC:        "testHeurekaCPC_2",
+			CategoryText:      "testCategoryText_2",
+			EAN:               "testEAN_2",
+			ProductNo:         "testProductNo_2",
+			Params: []models.ShopItemParam{
+				{XMLName: xml.Name{Local: "PARAM"}, ParamName: "testParam_2_1", Val: "testVal_2_1"},
+				{XMLName: xml.Name{Local: "PARAM"}, ParamName: "testParam_2_2", Val: "testVal_2_2"},
+			},
+			DelivaryDate: "testDelDate_2",
+			Deliveries: []models.ShopItemDelivery{
+				{XMLName: xml.Name{Local: "DELIVERY"}, DeliveryID: "testDelId_2_1", DeliveryPrice: "testDelPrice_2_1", DeliveryPriceCOD: "testDelPriceCOD_2_1"},
+				{XMLName: xml.Name{Local: "DELIVERY"}, DeliveryID: "testDelId_2_2", DeliveryPrice: "testDelPrice_2_2", DeliveryPriceCOD: "testDelPriceCOD_2_2"},
+			},
+			ItemGroupId: "testGroupId_2",
+			Accessory:   "testAccesory_2",
+			Gift:        "testGift_2",
+			ExtendedWarranty: []models.ShopItemExtendedWarranty{
+				{XMLName: xml.Name{Local: "EXTENDED_WARRANTY"}, Val: "testWarrantyVal_2_1", Desc: "testWarrantyDesc_2_1"},
+				{XMLName: xml.Name{Local: "EXTENDED_WARRANTY"}, Val: "testWarrantyVal_2_2", Desc: "testWarrantyDesc_2_2"},
+				{XMLName: xml.Name{Local: "EXTENDED_WARRANTY"}, Val: "testWarrantyVal_2_3", Desc: "testWarrantyDesc_2_3"},
+			},
+			SpecialService: "testSpecSvc_2",
+			SalesVoucher: []models.ShopItemSalesVoucher{
+				{XMLName: xml.Name{Local: "SALES_VOUCHER"}, Code: "testVoucherCode_2_1", Desc: "testVoucherDesc_2_1"},
+			},
+		},
+		{
+			XMLName:           xml.Name{Local: "SHOPITEM"},
+			ItemID:            "testId_3",
+			ProductName:       "testProductName_3",
+			Product:           "testProduct_3",
+			Description:       "testDescription_3",
+			Url:               "testUrl_3",
+			ImgUrl:            "testImgUrl_3",
+			ImgUrlAlternative: "testImgUrlAlt_3",
+			VideoUrl:          "testVideoUrl_3",
+			PriceVat:          "testPriceVat_3",
+			HeurekaCPC:        "testHeurekaCPC_3",
+			CategoryText:      "testCategoryText_3",
+			EAN:               "testEAN_3",
+			ProductNo:         "testProductNo_3",
+			Params: []models.ShopItemParam{
+				{XMLName: xml.Name{Local: "PARAM"}, ParamName: "testParam_3_1", Val: "testVal_3_1"},
+				{XMLName: xml.Name{Local: "PARAM"}, ParamName: "testParam_3_2", Val: "testVal_3_2"},
+			},
+			DelivaryDate: "testDelDate_3",
+			Deliveries: []models.ShopItemDelivery{
+				{XMLName: xml.Name{Local: "DELIVERY"}, DeliveryID: "testDelId_3_1", DeliveryPrice: "testDelPrice_3_1", DeliveryPriceCOD: "testDelPriceCOD_3_1"},
+				{XMLName: xml.Name{Local: "DELIVERY"}, DeliveryID: "testDelId_3_2", DeliveryPrice: "testDelPrice_3_2", DeliveryPriceCOD: "testDelPriceCOD_3_2"},
+			},
+			ItemGroupId: "testGroupId_3",
+			Accessory:   "testAccesory_3",
+			Gift:        "testGift_3",
+			ExtendedWarranty: []models.ShopItemExtendedWarranty{
+				{XMLName: xml.Name{Local: "EXTENDED_WARRANTY"}, Val: "testWarrantyVal_3_1", Desc: "testWarrantyDesc_3_1"},
+				{XMLName: xml.Name{Local: "EXTENDED_WARRANTY"}, Val: "testWarrantyVal_3_2", Desc: "testWarrantyDesc_3_2"},
+				{XMLName: xml.Name{Local: "EXTENDED_WARRANTY"}, Val: "testWarrantyVal_3_3", Desc: "testWarrantyDesc_3_3"},
+			},
+			SpecialService: "testSpecSvc_3",
+			SalesVoucher: []models.ShopItemSalesVoucher{
+				{XMLName: xml.Name{Local: "SALES_VOUCHER"}, Code: "testVoucherCode_3_1", Desc: "testVoucherDesc_3_1"},
+			},
+		},
+		{
+			XMLName:           xml.Name{Local: "SHOPITEM"},
+			ItemID:            "testId_3",
+			ProductName:       "testProductName_3",
+			Product:           "testProduct_3",
+			Description:       "testDescription_3",
+			Url:               "testUrl_3",
+			ImgUrl:            "testImgUrl_3",
+			ImgUrlAlternative: "testImgUrlAlt_3",
+			VideoUrl:          "testVideoUrl_3",
+			PriceVat:          "testPriceVat_3",
+			HeurekaCPC:        "testHeurekaCPC_3",
+			CategoryText:      "testCategoryText_3",
+			EAN:               "testEAN_3",
+			ProductNo:         "testProductNo_3",
+			Params: []models.ShopItemParam{
+				{XMLName: xml.Name{Local: "PARAM"}, ParamName: "testParam_3_1", Val: "testVal_3_1"},
+				{XMLName: xml.Name{Local: "PARAM"}, ParamName: "testParam_3_2", Val: "testVal_3_2"},
+			},
+			DelivaryDate: "testDelDate_3",
+			Deliveries: []models.ShopItemDelivery{
+				{XMLName: xml.Name{Local: "DELIVERY"}, DeliveryID: "testDelId_3_1", DeliveryPrice: "testDelPrice_3_1", DeliveryPriceCOD: "testDelPriceCOD_3_1"},
+				{XMLName: xml.Name{Local: "DELIVERY"}, DeliveryID: "testDelId_3_2", DeliveryPrice: "testDelPrice_3_2", DeliveryPriceCOD: "testDelPriceCOD_3_2"},
+			},
+			ItemGroupId: "testGroupId_3",
+			Accessory:   "testAccesory_3",
+			Gift:        "testGift_3",
+			ExtendedWarranty: []models.ShopItemExtendedWarranty{
+				{XMLName: xml.Name{Local: "EXTENDED_WARRANTY"}, Val: "testWarrantyVal_3_1", Desc: "testWarrantyDesc_3_1"},
+				{XMLName: xml.Name{Local: "EXTENDED_WARRANTY"}, Val: "testWarrantyVal_3_2", Desc: "testWarrantyDesc_3_2"},
+				{XMLName: xml.Name{Local: "EXTENDED_WARRANTY"}, Val: "testWarrantyVal_3_3", Desc: "testWarrantyDesc_3_3"},
+			},
+			SpecialService: "testSpecSvc_3",
+			SalesVoucher: []models.ShopItemSalesVoucher{
+				{XMLName: xml.Name{Local: "SALES_VOUCHER"}, Code: "testVoucherCode_3_1", Desc: "testVoucherDesc_3_1"},
+			},
+		},
+	},
 }
--- fileparser/xmlparser/xmlparser_test.go
+++ fileparser/xmlparser/xmlparser_test.go
@@ -1,17 +1,234 @@
 package xmlparser
 
-var mockedXmlFile = []byte("<SHOP><SHOPITEM><ITEM_ID><![CDATA[1]]></ITEM_ID><PRODUCTNAME><![CDATA[Test Product 01]]></PRODUCTNAME><PRODUCT><![CDATA[Test Product 01]]></PRODUCT><DESCRIPTION><![CDATA[Test Description 01]]></DESCRIPTION><URL><![CDATA[https://www.testurl.com]]></URL><IMGURL><![CDATA[https://testimgurl.com]]></IMGURL><MANUFACTURER><![CDATA[Test Manufacturer]]></MANUFACTURER><CATEGORYTEXT><![CDATA[Test Products]]></CATEGORYTEXT><EAN><![CDATA[4716123314660]]></EAN><PRODUCTNO><![CDATA[NF-F12 PWM]]></PRODUCTNO><DELIVERY_DATE><![CDATA[2]]></DELIVERY_DATE><PRICE_VAT><![CDATA[499]]></PRICE_VAT><IMGURL_ALTERNATIVE><![CDATA[https://testimgurl.com/2]]></IMGURL_ALTERNATIVE><DELIVERY><DELIVERY_ID><![CDATA[DPD]]></DELIVERY_ID><DELIVERY_PRICE><![CDATA[99]]></DELIVERY_PRICE><DELIVERY_PRICE_COD><![CDATA[144]]></DELIVERY_PRICE_COD></DELIVERY><DELIVERY><DELIVERY_ID><![CDATA[WEDO]]></DELIVERY_ID><DELIVERY_PRICE><![CDATA[99]]></DELIVERY_PRICE><DELIVERY_PRICE_COD><![CDATA[144]]></DELIVERY_PRICE_COD></DELIVERY><PARAM><PARAM_NAME><![CDATA[param_1]]></PARAM_NAME><VAL><![CDATA[val_1]]></VAL></PARAM><PARAM><PARAM_NAME><![CDATA[param_2]]></PARAM_NAME><VAL><![CDATA[val_2]]></VAL></PARAM><HEUREKA_CPC><![CDATA[6,1248]]></HEUREKA_CPC></SHOPITEM></SHOP>")
+import (
+	"encoding/xml"
+	"io"
+	"reflect"
+	"strings"
+	"testing"
 
-// func TestParseXmlFeed(t *testing.T) {
-// 	parser := XmlFeedParser{}
+	"github.com/MichalMitros/feed-parser/models"
+)
 
-// 	shop, err := parser.ParseFile(mockedXmlFile)
+func TestXmlFeedParser(t *testing.T) {
+	// Prepare mocked data
+	xmlFileString, _ := xml.Marshal(mockedCorrectShop)
+	mockedReadCloser := io.NopCloser(strings.NewReader(string(xmlFileString)))
+	output := make(chan models.ShopItem, len(mockedCorrectShop.ShopItems))
 
-// 	if err != nil {
-// 		t.Fatalf(`ParseFile(mockedXmlFile), err = %v, want 1`, err)
-// 	}
-// 	if len(shop.ShopItems) != 1 {
-// 		t.Fatalf(`ParseFeedXml(mockedXmlFile), len(result) = %q, want 1`, len(shop.ShopItems))
-// 	}
+	// Parse data
+	parser := NewXmlFeedParser()
+	parser.ParseFile(mockedReadCloser, output)
+	var results []models.ShopItem
+	for item := range output {
+		results = append(results, item)
+	}
 
-// }
+	// Check if all data are parsed
+	if len(results) != len(mockedCorrectShop.ShopItems) {
+		t.Fatalf(
+			`xmlparser.ParseFile(mockedCorrectShop, output), number of results = %d, want %d`,
+			len(results),
+			len(mockedCorrectShop.ShopItems),
+		)
+	}
+
+	// Check if all data are parsed correctly
+	for idx, result := range results {
+		if !reflect.DeepEqual(result, mockedCorrectShop.ShopItems[idx]) {
+			t.Fatalf(
+				"xmlparser.ParseFile(mockedCorrectShop, output), results[%d] = \n%v\n, want \n%v\n",
+				idx,
+				result,
+				mockedCorrectShop.ShopItems[idx],
+			)
+		}
+	}
+}
+
+func TestXmlFeedParserFailure(t *testing.T) {
+
+	// Prepare mocked data
+	xmlFileStringBytes, _ := xml.Marshal(mockedCorrectShop)
+	xmlFileString := string(xmlFileStringBytes)
+	// Break first <DELIVERY> tag closure
+	xmlFileString = strings.Join(
+		strings.SplitN(xmlFileString, "</DELIVERY>", 2),
+		"</WRONG_TAG_CLOSURE>",
+	)
+	mockedReadCloser := io.NopCloser(strings.NewReader(xmlFileString))
+	output := make(chan models.ShopItem, len(mockedCorrectShop.ShopItems))
+
+	// Parse data
+	parser := NewXmlFeedParser()
+	parser.ParseFile(mockedReadCloser, output)
+	var results []models.ShopItem
+	for item := range output {
+		results = append(results, item)
+	}
+
+	// Check if there is one partially parsed item
+	if len(results) != 1 {
+		t.Fatalf(
+			`xmlparser.ParseFile(mockedCorrectShop, output), number of results = %d, want %d`,
+			len(results),
+			1,
+		)
+	}
+}
+
+// MOCKED DATA
+
+// Correct shop with 3 items
+var mockedCorrectShop models.Shop = models.Shop{
+	ShopItems: []models.ShopItem{
+		{
+			XMLName:           xml.Name{Local: "SHOPITEM"},
+			ItemID:            "testId_1",
+			ProductName:       "testProductName_1",
+			Product:           "testProduct_1",
+			Description:       "testDescription_1",
+			Url:               "testUrl_1",
+			ImgUrl:            "testImgUrl_1",
+			ImgUrlAlternative: "testImgUrlAlt_1",
+			VideoUrl:          "testVideoUrl_1",
+			PriceVat:          "testPriceVat_1",
+			HeurekaCPC:        "testHeurekaCPC_1",
+			CategoryText:      "testCategoryText_1",
+			EAN:               "testEAN_1",
+			ProductNo:         "testProductNo_1",
+			Params: []models.ShopItemParam{
+				{XMLName: xml.Name{Local: "PARAM"}, ParamName: "testParam_1_1", Val: "testVal_1_1"},
+				{XMLName: xml.Name{Local: "PARAM"}, ParamName: "testParam_1_2", Val: "testVal_1_2"},
+			},
+			DelivaryDate: "testDelDate_1",
+			Deliveries: []models.ShopItemDelivery{
+				{XMLName: xml.Name{Local: "DELIVERY"}, DeliveryID: "testDelId_1_1", DeliveryPrice: "testDelPrice_1_1", DeliveryPriceCOD: "testDelPriceCOD_1_1"},
+				{XMLName: xml.Name{Local: "DELIVERY"}, DeliveryID: "testDelId_1_2", DeliveryPrice: "testDelPrice_1_2", DeliveryPriceCOD: "testDelPriceCOD_1_2"},
+			},
+			ItemGroupId: "testGroupId_1",
+			Accessory:   "testAccesory_1",
+			Gift:        "testGift_1",
+			ExtendedWarranty: []models.ShopItemExtendedWarranty{
+				{XMLName: xml.Name{Local: "EXTENDED_WARRANTY"}, Val: "testWarrantyVal_1_1", Desc: "testWarrantyDesc_1_1"},
+				{XMLName: xml.Name{Local: "EXTENDED_WARRANTY"}, Val: "testWarrantyVal_1_2", Desc: "testWarrantyDesc_1_2"},
+				{XMLName: xml.Name{Local: "EXTENDED_WARRANTY"}, Val: "testWarrantyVal_1_3", Desc: "testWarrantyDesc_1_3"},
+			},
+			SpecialService: "testSpecSvc_1",
+			SalesVoucher: []models.ShopItemSalesVoucher{
+				{XMLName: xml.Name{Local: "SALES_VOUCHER"}, Code: "testVoucherCode_1_1", Desc: "testVoucherDesc_1_1"},
+			},
+		},
+		{
+			XMLName:           xml.Name{Local: "SHOPITEM"},
+			ItemID:            "testId_2",
+			ProductName:       "testProductName_2",
+			Product:           "testProduct_2",
+			Description:       "testDescription_2",
+			Url:               "testUrl_2",
+			ImgUrl:            "testImgUrl_2",
+			ImgUrlAlternative: "testImgUrlAlt_2",
+			VideoUrl:          "testVideoUrl_2",
+			PriceVat:          "testPriceVat_2",
+			HeurekaCPC:        "testHeurekaCPC_2",
+			CategoryText:      "testCategoryText_2",
+			EAN:               "testEAN_2",
+			ProductNo:         "testProductNo_2",
+			Params: []models.ShopItemParam{
+				{XMLName: xml.Name{Local: "PARAM"}, ParamName: "testParam_2_1", Val: "testVal_2_1"},
+				{XMLName: xml.Name{Local: "PARAM"}, ParamName: "testParam_2_2", Val: "testVal_2_2"},
+			},
+			DelivaryDate: "testDelDate_2",
+			Deliveries: []models.ShopItemDelivery{
+				{XMLName: xml.Name{Local: "DELIVERY"}, DeliveryID: "testDelId_2_1", DeliveryPrice: "testDelPrice_2_1", DeliveryPriceCOD: "testDelPriceCOD_2_1"},
+				{XMLName: xml.Name{Local: "DELIVERY"}, DeliveryID: "testDelId_2_2", DeliveryPrice: "testDelPrice_2_2", DeliveryPriceCOD: "testDelPriceCOD_2_2"},
+			},
+			ItemGroupId: "testGroupId_2",
+			Accessory:   "testAccesory_2",
+			Gift:        "testGift_2",
+			ExtendedWarranty: []models.ShopItemExtendedWarranty{
+				{XMLName: xml.Name{Local: "EXTENDED_WARRANTY"}, Val: "testWarrantyVal_2_1", Desc: "testWarrantyDesc_2_1"},
+				{XMLName: xml.Name{Local: "EXTENDED_WARRANTY"}, Val: "testWarrantyVal_2_2", Desc: "testWarrantyDesc_2_2"},
+				{XMLName: xml.Name{Local: "EXTENDED_WARRANTY"}, Val: "testWarrantyVal_2_3", Desc: "testWarrantyDesc_2_3"},
+			},
+			SpecialService: "testSpecSvc_2",
+			SalesVoucher: []models.ShopItemSalesVoucher{
+				{XMLName: xml.Name{Local: "SALES_VOUCHER"}, Code: "testVoucherCode_2_1", Desc: "testVoucherDesc_2_1"},
+			},
+		},
+		{
+			XMLName:           xml.Name{Local: "SHOPITEM"},
+			ItemID:            "testId_3",
+			ProductName:       "testProductName_3",
+			Product:           "testProduct_3",
+			Description:       "testDescription_3",
+			Url:               "testUrl_3",
+			ImgUrl:            "testImgUrl_3",
+			ImgUrlAlternative: "testImgUrlAlt_3",
+			VideoUrl:          "testVideoUrl_3",
+			PriceVat:          "testPriceVat_3",
+			HeurekaCPC:        "testHeurekaCPC_3",
+			CategoryText:      "testCategoryText_3",
+			EAN:               "testEAN_3",
+			ProductNo:         "testProductNo_3",
+			Params: []models.ShopItemParam{
+				{XMLName: xml.Name{Local: "PARAM"}, ParamName: "testParam_3_1", Val: "testVal_3_1"},
+				{XMLName: xml.Name{Local: "PARAM"}, ParamName: "testParam_3_2", Val: "testVal_3_2"},
+			},
+			DelivaryDate: "testDelDate_3",
+			Deliveries: []models.ShopItemDelivery{
+				{XMLName: xml.Name{Local: "DELIVERY"}, DeliveryID: "testDelId_3_1", DeliveryPrice: "testDelPrice_3_1", DeliveryPriceCOD: "testDelPriceCOD_3_1"},
+				{XMLName: xml.Name{Local: "DELIVERY"}, DeliveryID: "testDelId_3_2", DeliveryPrice: "testDelPrice_3_2", DeliveryPriceCOD: "testDelPriceCOD_3_2"},
+			},
+			ItemGroupId: "testGroupId_3",
+			Accessory:   "testAccesory_3",
+			Gift:        "testGift_3",
+			ExtendedWarranty: []models.ShopItemExtendedWarranty{
+				{XMLName: xml.Name{Local: "EXTENDED_WARRANTY"}, Val: "testWarrantyVal_3_1", Desc: "testWarrantyDesc_3_1"},
+				{XMLName: xml.Name{Local: "EXTENDED_WARRANTY"}, Val: "testWarrantyVal_3_2", Desc: "testWarrantyDesc_3_2"},
+				{XMLName: xml.Name{Local: "EXTENDED_WARRANTY"}, Val: "testWarrantyVal_3_3", Desc: "testWarrantyDesc_3_3"},
+			},
+			SpecialService: "testSpecSvc_3",
+			SalesVoucher: []models.ShopItemSalesVoucher{
+				{XMLName: xml.Name{Local: "SALES_VOUCHER"}, Code: "testVoucherCode_3_1", Desc: "testVoucherDesc_3_1"},
+			},
+		},
+		{
+			XMLName:           xml.Name{Local: "SHOPITEM"},
+			ItemID:            "testId_3",
+			ProductName:       "testProductName_3",
+			Product:           "testProduct_3",
+			Description:       "testDescription_3",
+			Url:               "testUrl_3",
+			ImgUrl:            "testImgUrl_3",
+			ImgUrlAlternative: "testImgUrlAlt_3",
+			VideoUrl:          "testVideoUrl_3",
+			PriceVat:          "testPriceVat_3",
+			HeurekaCPC:        "testHeurekaCPC_3",
+			CategoryText:      "testCategoryText_3",
+			EAN:               "testEAN_3",
+			ProductNo:         "testProductNo_3",
+			Params: []models.ShopItemParam{
+				{XMLName: xml.Name{Local: "PARAM"}, ParamName: "testParam_3_1", Val: "testVal_3_1"},
+				{XMLName: xml.Name{Local: "PARAM"}, ParamName: "testParam_3_2", Val: "testVal_3_2"},
+			},
+			DelivaryDate: "testDelDate_3",
+			Deliveries: []models.ShopItemDelivery{
+				{XMLName: xml.Name{Local: "DELIVERY"}, DeliveryID: "testDelId_3_1", DeliveryPrice: "testDelPrice_3_1", DeliveryPriceCOD: "testDelPriceCOD_3_1"},
+				{XMLName: xml.Name{Local: "DELIVERY"}, DeliveryID: "testDelId_3_2", DeliveryPrice: "testDelPrice_3_2", DeliveryPriceCOD: "testDelPriceCOD_3_2"},
+			},
+			ItemGroupId: "testGroupId_3",
+			Accessory:   "testAccesory_3",
+			Gift:        "testGift_3",
+			ExtendedWarranty: []models.ShopItemExtendedWarranty{
+				{XMLName: xml.Name{Local: "EXTENDED_WARRANTY"}, Val: "testWarrantyVal_3_1", Desc: "testWarrantyDesc_3_1"},
+				{XMLName: xml.Name{Local: "EXTENDED_WARRANTY"}, Val: "testWarrantyVal_3_2", Desc: "testWarrantyDesc_3_2"},
+				{XMLName: xml.Name{Local: "EXTENDED_WARRANTY"}, Val: "testWarrantyVal_3_3", Desc: "testWarrantyDesc_3_3"},
+			},
+			SpecialService: "testSpecSvc_3",
+			SalesVoucher: []models.ShopItemSalesVoucher{
+				{XMLName: xml.Name{Local: "SALES_VOUCHER"}, Code: "testVoucherCode_3_1", Desc: "testVoucherDesc_3_1"},
+			},
+		},
+	},
+}
